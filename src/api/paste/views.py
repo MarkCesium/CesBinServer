c81d4cc59fed36664d9a116d4a1ce757e9578415
MarkCesium <cesiummark01@gmail.com>
2024-06-10 01:56:50 +0300

    Add views in paste package, export in main

---
+++ src/api/paste/views.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from . import crud, schemas
+from src.core.db_helper import db_helper
+from src.core.models import Paste
+from uuid import uuid4
+from time import time
+
+router = APIRouter(prefix="/paste", tags=["Paste"])
+
+
+@router.get("/{id}")
+async def get_paste(
+    id: str,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> schemas.Paste:
+    paste = await crud.get_paste(session, id)
+    if paste is not None:
+        return paste
+    raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+
+@router.post("/")
+async def get_paste(
+    data: schemas.PasteCreate,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> schemas.Paste:
+    return await crud.create_paste(
+        session,
+        Paste(**data.model_dump(), id=str(uuid4()), created_at=round(time() * 1000)),
+    )
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from src.api import paste_router
 
 app = FastAPI()
+app.include_router(paste_router)
 
 
 @app.get("/")
