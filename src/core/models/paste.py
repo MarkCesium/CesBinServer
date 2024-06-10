beda77741fbae6d7c5f4b12bfa550994b5481488
MarkCesium <cesiummark01@gmail.com>
2024-06-10 17:27:10 +0300

    Update Paste model

--- src/core/models/paste.py
+++ src/core/models/paste.py
@@ -1,7 +1,10 @@
 from .base import Base
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
 
 
 class Paste(Base):
-    text: Mapped[str]
-    created_at: Mapped[int]
+    title: Mapped[str] = mapped_column(String(20), nullable=False)
+    text: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[int] = mapped_column(Integer, nullable=False)
+    expire_at: Mapped[int | None] = mapped_column(Integer, nullable=True)
