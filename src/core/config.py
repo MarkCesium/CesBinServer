8ece30ee8a61f8d90b9ed19ab419782bc1008683
MarkCesium <danikhrenkov29@gmail.com>
2024-10-05 16:07:47 +0300

    Update Paste, edit ID, edit DSN

--- src/core/config.py
+++ src/core/config.py
@@ -5,8 +5,12 @@ BASE_DIR = Path(__file__).parent.parent.parent
 
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    db_url: str | None = None
+    db_user: str
+    db_pass: str
+    db_name: str
     db_echo: bool = False
 
 
 settings = Settings()
+settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_pass}@cesbin-database/{settings.db_name}"
--- src/core/models/paste.py
+++ src/core/models/paste.py
@@ -1,9 +1,13 @@
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
 
 
 class Paste(Base):
     paste_path: Mapped[str] = mapped_column(String(120), nullable=False)
     created_at: Mapped[int] = mapped_column(Integer, nullable=False)
     expire_at: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    period_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("Period.id"), nullable=True
+    )
+    expiration_period = relationship("Period", back_populates="Paste")
