c590bf5358d1e56c5ed3c055e88aa2d435d45430
MarkCesium <cesiummark01@gmail.com>
2024-11-16 17:01:43 +0300

    Edit config

--- src/core/config.py
+++ src/core/config.py
@@ -8,9 +8,10 @@ class Settings(BaseSettings):
     db_url: str | None = None
     db_user: str
     db_pass: str
+    db_host: str
     db_name: str
     db_echo: bool = False
 
 
 settings = Settings()
-settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_pass}@cesbin-database/{settings.db_name}"
+settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_pass}@{settings.db_host}/{settings.db_name}"
