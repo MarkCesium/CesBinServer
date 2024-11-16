f2cc7f787d5b4054a88d67fca7f24db3b6858555
MarkCesium <cesiummark01@gmail.com>
2024-11-16 20:40:42 +0300

    Integrate paste service into views

--- src/api/paste/schemas.py
+++ src/api/paste/schemas.py
@@ -1,27 +1,15 @@
-from pydantic import BaseModel, ConfigDict
-from enum import Enum
-
-
-class Period(str, Enum):
-    # TODO: Rewrite! Get Periods from DataBase
-    one_hour = "1 hour"
-    three_hours = "3 hours"
-    one_day = "1 day"
-    one_week = "1 week"
-    one_month = "1 month"
-    three_months = "3 months"
-    six_months = "6 months"
-    one_year = "1 year"
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
 
 
 class PasteCreate(BaseModel):
     text: str
-    period: Period | None = None
+    period: str | None = Field(default=None, min_length=5, max_length=20)
 
 
 class PasteRead(BaseModel):
     id: int
     text: str
-    created_at: int
-    expire_at: int | None = None
-    model_config = ConfigDict(from_attributes=True)
+    created_at: datetime
+    expire_at: datetime | None = None
+    # model_config = ConfigDict(from_attributes=True)
--- src/api/paste/views.py
+++ src/api/paste/views.py
@@ -1,18 +1,35 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from . import crud, schemas
-from .dependency import get_paste_dependency
+from time import time
+
+from . import schemas
+from src.core.config import BASE_DIR
 from src.core.db_helper import db_helper
+from src.core.repositories import PasteRepository
 from src.core.models import Paste
+from src.services.paste import PasteService
+from src.services.files import FileService
 
 router = APIRouter(prefix="/paste", tags=["Paste"])
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK)
 async def get_paste(
-    paste: schemas.PasteRead = Depends(get_paste_dependency),
+    id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> schemas.PasteRead:
-    return paste
+    paste: Paste = await PasteRepository.get_by_id(session, id)
+    if paste is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    text = await FileService.get(str(BASE_DIR / paste.path))
+    return schemas.PasteRead(
+        id=paste.id,
+        text=text,
+        created_at=paste.created_at,
+        expire_at=paste.expire_at if paste.expire_at is not None else None,
+    )
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -20,4 +37,13 @@ async def create_paste(
     paste: schemas.PasteCreate,
     session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> schemas.PasteRead:
-    return await crud.create_paste(session, paste)
+    id = await PasteService.create(session, paste.text, paste.period)
+    paste: Paste = await PasteRepository.get_by_id(session, id)
+    text = await FileService.get(str(BASE_DIR / paste.path))
+
+    return schemas.PasteRead(
+        id=paste.id,
+        text=text,
+        created_at=paste.created_at,
+        expire_at=paste.expire_at if paste.expire_at is not None else None,
+    )
