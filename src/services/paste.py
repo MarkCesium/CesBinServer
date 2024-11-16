74f4c5256db3044e0520939a6e65fc9814cf2a1d
MarkCesium <cesiummark01@gmail.com>
2024-11-16 22:00:36 +0300

    Integrate format, edit services and views

--- src/services/paste.py
+++ src/services/paste.py
@@ -4,25 +4,42 @@ from fastapi import status
 from time import time
 
 from src.services.files import FileService
-from src.core.models import Paste, Period
-from src.core.repositories import PeriodRepository
+from src.core.models import Paste, Period, Format
+from src.core.repositories import PeriodRepository, FormatRepository, PasteRepository
 from src.core.config import BASE_DIR
 
 
 class PasteService:
     @classmethod
     async def create(
-        cls, session: AsyncSession, text: str, period_name: str | None = None
+        cls,
+        session: AsyncSession,
+        text: str,
+        format_name: str,
+        period_name: str | None = None,
     ) -> int:
         paste = Paste()
         path = f"pastes/{time()*1000}.txt"
         paste.path = str(path)
+
+        format: Format | None = await FormatRepository.get_one_or_none(
+            session, name=format_name
+        )
+        if format is None:
+            await session.close()
+            raise HTTPException(status.HTTP_404_NOT_FOUND, "Format not found")
+
+        paste.format_id = format.id
+
+        session.add(paste)
+        await session.flush([paste])
 
         if period_name is not None:
             period: Period = await PeriodRepository.get_one_or_none(
                 session, name=period_name
             )
             if period is None:
+                await session.close()
                 raise HTTPException(status.HTTP_404_NOT_FOUND, "Period not found")
             paste.expire_at = paste.created_at + period.duration
 
@@ -32,7 +49,23 @@ class PasteService:
             await session.close()
             raise HTTPException(status.HTTP_400_BAD_REQUEST)
 
-        session.add(paste)
         await session.commit()
 
         return paste.id
+
+    @classmethod
+    async def get(cls, session: AsyncSession, id: int) -> str:
+        paste: Paste = await PasteRepository.get_by_id(session, id)
+        if paste is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        text: str = await FileService.get(str(BASE_DIR / paste.path))
+        format: Format = await FormatRepository.get_by_id(session, paste.format_id)
+
+        return {
+            "id": paste.id,
+            "text": text,
+            "created_at": paste.created_at,
+            "expire_at": paste.expire_at if paste.expire_at is not None else None,
+            "format": format.name,
+        }
