5cbb79118ec3acb459bf7988a1e832c5d1833e3b
MarkCesium <cesiummark01@gmail.com>
2024-11-16 20:38:32 +0300

    Re-write service of files

--- src/services/files.py
+++ src/services/files.py
@@ -1,13 +1,14 @@
 from aiofile import async_open
-from src.core.config import BASE_DIR
 
 
-async def get_paste_from_file(paste_path: str) -> str:
-    async with async_open(paste_path, "rb") as file:
-        return await file.read()
+class FileService:
+    @classmethod
+    async def get(cls, path: str) -> str:
+        async with async_open(path, "rb") as file:
+            return await file.read()
 
-
-async def create_paste_file(paste_path: str, text: str) -> None:
-    async with async_open(paste_path, "wb") as file:
-        await file.write(text.encode())
-        file.seek(0)
+    @classmethod
+    async def create(cls, path: str, text: str) -> None:
+        async with async_open(path, "wb") as file:
+            await file.write(text.encode())
+            file.seek(0)
