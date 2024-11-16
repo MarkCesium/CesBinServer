d6fc564bf4e2d8f6fa82d8fb281b1af2606b6201
MarkCesium <cesiummark01@gmail.com>
2024-11-16 20:37:56 +0300

    Edit Paste model

--- src/core/models/paste.py
+++ src/core/models/paste.py
@@ -1,9 +1,10 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy import String, DateTime, func
+from datetime import datetime
 
 
 class Paste(Base):
-    paste_path: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
-    created_at: Mapped[int] = mapped_column(Integer, nullable=False)
-    expire_at: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    path: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(True), server_default="NOW()")
+    expire_at: Mapped[datetime | None] = mapped_column(DateTime(True), nullable=True)
