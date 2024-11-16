4a6a04bfa30d368676c1e9d8fbfb918321429883
MarkCesium <cesiummark01@gmail.com>
2024-11-16 21:58:40 +0300

    Create format model and migration

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,9 +1,12 @@
+from .base import Base
+from .paste import Paste
+from .period import Period
+from .format import Format
+
+
 __all__ = (
     "Base",
     "Paste",
     "Period",
+    "Format",
 )
-
-from .base import Base
-from .paste import Paste
-from .period import Period
---
+++ src/core/models/format.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from sqlalchemy import String
+from typing import List
+
+from .base import Base
+
+
+class Format(Base):
+    name: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    pastes = relationship("Paste", back_populates="format")
