a7fa4887bf8d26e0c8cb9b7970fa272c07b61a3c
MarkCesium <cesiummark01@gmail.com>
2024-11-16 17:00:54 +0300

    Edit models

--- src/core/models/paste.py
+++ src/core/models/paste.py
@@ -1,13 +1,9 @@
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
 
 
 class Paste(Base):
-    paste_path: Mapped[str] = mapped_column(String(120), nullable=False)
+    paste_path: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
     created_at: Mapped[int] = mapped_column(Integer, nullable=False)
     expire_at: Mapped[int | None] = mapped_column(Integer, nullable=True)
-    period_id: Mapped[int | None] = mapped_column(
-        Integer, ForeignKey("Period.id"), nullable=True
-    )
-    expiration_period = relationship("Period", back_populates="Paste")
--- src/core/models/period.py
+++ src/core/models/period.py
@@ -1,9 +1,8 @@
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, Interval
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Interval
 
 
 class Period(Base):
-    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    name: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
     duration: Mapped[Interval] = mapped_column(Interval, nullable=False)
-    paste = relationship("Paste", back_populates="Period")
