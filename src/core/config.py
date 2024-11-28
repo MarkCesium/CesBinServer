751abcf890de943048f12d0bce1045fafcb4584e
MarkCesium <danikhrenkov29@gmail.com>
2024-11-28 22:34:06 +0300

    Update config and env ex

--- src/core/config.py
+++ src/core/config.py
@@ -7,10 +7,15 @@ BASE_DIR = Path(__file__).parent.parent.parent
 class Settings(BaseSettings):
     db_url: str | None = None
     db_user: str
-    db_pass: str
+    db_password: str
     db_host: str
     db_name: str
     db_echo: bool = False
+
+    rabbitmq_url: str | None = None
+    rabbitmq_host: str
+    rabbitmq_user: str
+    rabbitmq_password: str
 
     class Config:
         env_file = BASE_DIR / ".env"
@@ -18,4 +23,5 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_pass}@{settings.db_host}/{settings.db_name}"
+settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
+settings.rabbitmq_url = f"pyamqp://{settings.rabbitmq_user}:{settings.rabbitmq_password}@{settings.rabbitmq_host}"
