c0f073d012a78a549bfe845efe5eb4ccffacbaf0
MarkCesium <danikhrenkov29@gmail.com>
2024-11-29 11:25:29 +0300

    Create sync base repo, update config

--- src/api/paste/views.py
+++ src/api/paste/views.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/paste", tags=["Paste"])
 @router.get("/{id}", status_code=status.HTTP_200_OK)
 async def get_paste(
     id: int,
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(db_helper.async_session_dependency),
 ) -> schemas.PasteRead:
     return await PasteService.get(session, id)
 
@@ -19,7 +19,7 @@ async def get_paste(
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_paste(
     paste: schemas.PasteCreate,
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(db_helper.async_session_dependency),
 ) -> schemas.PasteRead:
     id = await PasteService.create(session, paste.text, paste.format, paste.period)
 
--- src/core/config.py
+++ src/core/config.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).parent.parent.parent
 
 class Settings(BaseSettings):
     db_url: str | None = None
+    db_sync_url: str | None = None
     db_user: str
     db_password: str
     db_host: str
@@ -24,4 +25,5 @@ class Settings(BaseSettings):
 
 settings = Settings()
 settings.db_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
+settings.db_sync_url = f"postgresql+psycopg://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
 settings.rabbitmq_url = f"pyamqp://{settings.rabbitmq_user}:{settings.rabbitmq_password}@{settings.rabbitmq_host}"
